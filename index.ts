9e56243234c43b82e7a8691c2b74b8005aab8f9f
alan.cao <zongying.cao@hpe.com>
2018-06-21 06:46:06 -0400

    add code to apply rest
    Signed-off-by: alan.cao <zongying.cao@hpe.com>

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import { Observable } from 'rxjs/Observable';
 import { Observer } from 'rxjs/Observer';
 import { Http } from '@angular/http';
 import { Injectable, InjectionToken, Inject } from '@angular/core';
+import * as _ from 'underscore';
 
 export interface MicroServiceAPIConfig {
     baseUrl: string
@@ -11,7 +12,7 @@ export const MICRO_API_CONF = new InjectionToken<MicroServiceAPIConfig>("microse
 
 
 export abstract class Service{
-    abstract post(microserviceName: string, methodName: string, param: any): Observable<any>;
+    abstract act(microserviceName: string, methodName: string, id: string, param: any): Observable<any>;
 }
 
 @Injectable()
@@ -20,29 +21,76 @@ export class MicroserviceClient extends Service{
     constructor(private http: Http, @Inject(MICRO_API_CONF) apiConfig: MicroServiceAPIConfig){
       super();
       this.buildUrl = (microserviceName:string):string=>{
-        return `${apiConfig.baseUrl}/_api/${microserviceName}/act`;
+        return `${apiConfig.baseUrl}/_api/${microserviceName}`;
       };
     }
-    post(microserviceName: string, methodName: string, param: any): Observable<any>{
+    act(microserviceName: string, methodName: string, id: any, param: any): Observable<any>{
 
-    	return new Observable<any>((observer: Observer<any>)=>{
-	    this.http.post(this.buildUrl(microserviceName), {
+        let rest_methods = ["get", "post", "delete", "patch"];
+
+        function is_rest() {
+          return methodName.toLowerCase() in rest_methods;
+        }
+
+        function build_rest_url() {
+          return ((dict)=>{
+	    return dict[methodName.toLowerCase()]();
+	  })(
+	  {
+            "get": function(){
+	      return param ? `${this.buildUrl(microserviceName)}/${id}` : `${this.buildUrl(microserviceName)}`;
+	    },
+	    "post": function(){
+              return this.buildUrl(microserviceName);
+            },
+	    "delete": function(){
+              return `${this.buildUrl(microserviceName)}/${id}`;
+            },
+	    "patch": function(){
+	      return `${this.buildUrl(microserviceName)}/${id}`;
+            }
+          }
+	  );
+        }
+
+        let rest_actions = {
+          "get": function(){
+            return this.http.get(build_rest_url());
+          },
+	  "post": function(){
+	    return this.http.post(build_rest_url(), param);
+	  },
+	  "delete": function(){
+	    return this.http.delete(build_rest_url());
+	  },
+	  "patch": function(){
+	    return this.http.patch(build_rest_url(), param);
+	  }
+        };
+
+        return is_rest() ? new Observable<any>((observer: Observer<any>)=>{
+	  rest_actions[methodName.toLowerCase()]().
+	    subscribe(res=>{
+	      observer.next(res);
+	    });
+	  
+	}) :
+	    new Observable<any>((observer: Observer<any>)=>{
+	      this.http.post(this.buildUrl(microserviceName), {
 		action: methodName,
 		param: param
-
-	    })
-	    .subscribe((res)=>{
-	        observer.next("_body" in res ? JSON.parse(res["_body"]) : res);
-	    });
+		}).subscribe((res)=>{
+                     observer.next("_body" in res ? JSON.parse(res["_body"]) : res);
+	        });
 	});
-    
+
     }
 }
 
-export function useService(service: Service, microserviceName: string): (string)=>(any)=>Observable<any>{
-    return (methodName: string): (any)=>Observable<any> =>{
-        return (param: any): Observable<any> =>{
-	    return service.post(microserviceName, methodName, param);
+export function useService(service: Service, microserviceName: string): (string)=>(any, any)=>Observable<any>{
+    return (methodName: string): (any, any)=>Observable<any> =>{
+        return (id: any, param: any): Observable<any> =>{
+	    return service.act(microserviceName, methodName, id, param);
         };
     };
 }
